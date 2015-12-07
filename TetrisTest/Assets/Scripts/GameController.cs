02e3d09fa5f94ac017837f9d24be6f909e102040
LeMatou <stratulatmark@yandex.ru>
2015-12-07 22:44:58 +0300

    First steps

---
+++ TetrisTest/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public const int HORIZONTAL_SIZE = 10;
+    public const int VERTICAL_SIZE = 20;
+    public const int ADDITIONAL_FIELD = 4;
+
+    public const int EMPTY_ELEMENT = 0;
+    public const int FREE_ELEMENT = 1;
+    public const int FIXED_ELEMENT = 2;
+
+    public RectTransform gameBackGround;
+
+    private int[,] gameAreaModel = new int[VERTICAL_SIZE + ADDITIONAL_FIELD, HORIZONTAL_SIZE];
+    private Image[,] gameAreaView = new Image[VERTICAL_SIZE + ADDITIONAL_FIELD, HORIZONTAL_SIZE];
+
+    void Start () {
+        Utilz.FillArea( gameBackGround, gameAreaView, HORIZONTAL_SIZE, VERTICAL_SIZE, ADDITIONAL_FIELD );
+        Utilz.UpdateView( gameAreaModel, gameAreaView, HORIZONTAL_SIZE, VERTICAL_SIZE, ADDITIONAL_FIELD );
+	}
+}
---
+++ TetrisTest/Assets/Scripts/Utilz.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class Utilz {
+
+    public static void FillArea( RectTransform rect, Image[,] gameAreaView, int horizontal, int vertical, int additionalField ) {
+        float verticalSize = rect.rect.size.x / horizontal;
+        float horizontalSize = rect.rect.size.x / horizontal;
+        for( int i = 0; i < vertical + additionalField; ++i ) {
+            for( int j = 0; j < horizontal; ++j ) {
+                GameObject go = new GameObject( "rect[" + i.ToString() + ", " + j.ToString() + "]", typeof(RectTransform), typeof(Image) );
+                RectTransform subRect = go.GetComponent<RectTransform>();
+                subRect.SetSizeWithCurrentAnchors( RectTransform.Axis.Horizontal, horizontalSize - 2.0f );
+                subRect.SetSizeWithCurrentAnchors( RectTransform.Axis.Vertical, verticalSize - 2.0f );
+                subRect.SetParent( rect );
+                subRect.localPosition = new Vector3( j * verticalSize + verticalSize / 2.0f, i * horizontalSize + horizontalSize / 2.0f, 0.0f );
+                gameAreaView[i, j] = subRect.GetComponent<Image>();
+            }
+        }
+    }
+
+    public static void UpdateView( int[,] gameAreaModel, Image[,] gameAreaView, int horizontal, int vertical, int additionalField ) {
+        for( int i = 0; i < vertical + additionalField; ++i ) {
+            for( int j = 0; j < horizontal; ++j ) {
+                gameAreaView[i, j].enabled = gameAreaModel[i, j] != 0;
+            }
+        }
+    }
+
+    public static bool IsBottom( int[,] gameAreaView, int horizontal, int vertical, int additionalField ) {
+        for( int i = 0; i < vertical + additionalField; ++i ) {
+            for( int j = 0; j < horizontal; ++j ) {
+                // ...
+            }
+        }
+        return false;
+    }
+}
