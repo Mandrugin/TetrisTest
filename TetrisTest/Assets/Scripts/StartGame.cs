0accccbd83c1623c5ff5a2da6fb418c72e92dc5e
v.mandrugin <v.mandrugin@areo-gaming.com>
2015-12-09 16:00:14 +0300

    Added start scene

---
+++ TetrisTest/Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour {
+
+	public Button StartButton;
+	public Button ExitButton;
+
+	void Awake() {
+		StartButton.onClick.AddListener( () => { Application.LoadLevel( "main" ); } );
+		ExitButton.onClick.AddListener( () => { Application.Quit(); } );
+	}
+}
